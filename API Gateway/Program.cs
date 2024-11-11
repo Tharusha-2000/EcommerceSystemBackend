64114d16a5a12604bf4213d1c9f13c9925688a71
Lahiru Nanayakkara <lahirunanayakkara924@gmail.com>
2024-11-12 00:08:17 +0530

    second

--- API Gateway/Program.cs
+++ API Gateway/Program.cs
@@ -1,11 +1,16 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
-builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddOcelot(builder.Configuration)
+    .AddCacheManager(x =>
+    {
+        x.WithDictionaryHandle();
+    });
 
 
 var app = builder.Build();
