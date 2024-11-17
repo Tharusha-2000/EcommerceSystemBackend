01dce7ab238f52c037d66b4ffdf4ba147f0f96b0
DilshanPathegamage123 <141545307+DilshanPathegamage123@users.noreply.github.com>
2024-11-17 17:12:27 +0530

    Review and Rating service is created. Migrations are working All set to developments

---
+++ userManage/Ecommerce.ReviewAndRating.Infrastructure/Migrations/20241117110913_Migration2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.ReviewAndRating.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TravelSearch");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TravelSearch",
+                columns: table => new
+                {
+                    feedbackId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    feedbackMessage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    givenDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    rate = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TravelSearch", x => x.feedbackId);
+                });
+        }
+    }
+}
