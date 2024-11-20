be41cc8bca680994efadfb18e8eb3141c62f15f4
Lahiru Nanayakkara <lahirunanayakkara924@gmail.com>
2024-11-20 16:01:26 +0530

    product manage files added

---
+++ Ecommerce/Ecommerce.ProductManage.Domain/Models/DTOs/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.ProductManage.Domain.Models.Domains;
+
+namespace Ecommerce.ProductManage.Domain.Models.DTOs
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public List<string> Categories { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public ICollection<ProductSize> Sizes { get; set; } = new List<ProductSize>();
+    }
+}
