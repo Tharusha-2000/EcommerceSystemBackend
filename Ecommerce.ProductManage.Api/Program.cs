16303de2634f243ccdce477dfac99525ddd448c2
Lahiru Nanayakkara <lahirunanayakkara924@gmail.com>
2024-11-20 13:13:40 +0530

    product manage service

--- Ecommerce.ProductManage.Api/Program.cs
+++ Ecommerce.ProductManage.Api/Program.cs
@@ -1,6 +1,25 @@
+using Ecommerce.ProductManage.Application.Services;
+using Ecommerce.ProductManage.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDb")
+    ));
+
+builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true; // Optional: makes JSON output more readable
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
