73d2a77279f913e68d41a27fa3ad5ac3c11a2da0
Nipun Malinga Premalal <nipunmalinga5@gmail.com>
2024-11-20 16:08:12 +0530

    new

---
+++ Ecommerce/Ecommerce.OrderProcessing.Domain/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Ecommerce.OrderProcessing.Domain.Models
+{
+    public class Order
+    {
+        [Key]
+        public int orderId { get; set; }
+        public int userId { get; set; }
+        public float totalPrice { get; set; }
+        public string paymentMethod { get; set; }
+        public string type { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public int postalcode { get; set; }
+        public string phoneNum { get; set; }
+        public bool paymentStatus { get; set; }
+        public string OrderStatus { get; set; }
+    }
+}
