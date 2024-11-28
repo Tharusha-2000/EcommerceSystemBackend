aa29d5b8aea3174190a0b116b6ec34631126815a
DilshanPathegamage123 <141545307+DilshanPathegamage123@users.noreply.github.com>
2024-11-29 01:41:10 +0530

    New API is added

---
+++ Ecommerce/Ecommerce.ReviewAndRating.Api/Controllers/GetFeedbackByOrderIdController.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.ReviewAndRating.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.ReviewAndRating.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetFeedbackByOrderIdController : ControllerBase
+    {
+        private readonly IReviewAndRatingService _reviewAndRatingService;
+
+        public GetFeedbackByOrderIdController(IReviewAndRatingService reviewAndRatingService)
+        {
+            _reviewAndRatingService = reviewAndRatingService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFeedbackByOrderId(int orderId)
+        {
+            var feedback = await _reviewAndRatingService.GetFeedbackByOrderId(orderId);
+            return Ok(feedback);
+        }
+    }
+
+}
