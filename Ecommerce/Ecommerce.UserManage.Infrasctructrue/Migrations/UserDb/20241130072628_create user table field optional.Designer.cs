7b847569d9317fac7c1cac5f24b4e43bad4da471
Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>
2024-11-30 16:16:16 +0530

    database required field updated and firstname added to the token

--- Ecommerce/Ecommerce.UserManage.Infrasctructrue/Migrations/UserDb/20241130072628_create user table field optional.Designer.cs
+++ Ecommerce/Ecommerce.UserManage.Infrasctructrue/Migrations/UserDb/20241130072628_create user table field optional.Designer.cs
@@ -3,6 +3,7 @@ using Ecommerce.userManage.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ecommerce.userManage.Infrastructure.Migrations.UserDb
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241130072628_create user table field optional")]
+    partial class createusertablefieldoptional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
