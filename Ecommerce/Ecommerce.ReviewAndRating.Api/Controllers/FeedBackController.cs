68c1fba7fd661616e532f8b21accb0dc8d634f3c
DilshanPathegamage123 <141545307+DilshanPathegamage123@users.noreply.github.com>
2024-11-30 23:24:27 +0530

    Controller Structure is chanded.

---
+++ Ecommerce/Ecommerce.ReviewAndRating.Api/Controllers/FeedBackController.cs
@@ -0,0 +1,41 @@
+﻿using Ecommerce.ReviewAndRating.Application.Services;
+using Ecommerce.ReviewAndRating.Domain.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.ReviewAndRating.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedBackController : ControllerBase
+    {
+
+        private IReviewAndRatingService _reviewAndRatingService;
+
+        public FeedBackController(IReviewAndRatingService reviewAndRatingService)
+        {
+            _reviewAndRatingService = reviewAndRatingService;
+        }
+
+        [HttpGet("GetProductFeedback/{productId:int}")]
+        public async Task<IActionResult> GetProductFeedback(int productId)
+        {
+            var feedbacks = await _reviewAndRatingService.GetProductFeedback(productId);
+            return Ok(feedbacks);
+        }
+
+        [HttpGet("GetFeedbackByOrderId/{orderId:int}")]
+        public async Task<IActionResult> GetFeedbackByOrderId(int orderId)
+        {
+            var feedback = await _reviewAndRatingService.GetFeedbackByOrderId(orderId);
+            return Ok(feedback);
+        }
+
+        [HttpPost("SaveProductFeedback")]
+        public async Task<IActionResult> SaveProductFeedback(FeedbackRequestDto feedbackDto)
+        {
+            await _reviewAndRatingService.SaveProductFeedback(feedbackDto);
+            return Ok();
+        }
+    }
+}
